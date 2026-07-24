484728ef20a5ca45df6347c934d8e62ad14762d5
Ashutosh Raj <ashutoshsharma5161@gmail.com>
2026-07-24 14:31:29 +0530

    configure backend with zoho catalyst appsail

--- src/backend/main.py
+++ src/backend/main.py
@@ -133,4 +133,6 @@ app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    import os
+    port = int(os.environ.get("X_ZOHO_CATALYST_LISTEN_PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True, log_level="info")
